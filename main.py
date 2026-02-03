4e25d95c035339efa8600d19d893bdce33c7d500
moon <csteel217@gmail.com>
2026-02-04 12:23:55 +1300

    add system prompt

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 import os
 import argparse
+from prompts import system_prompt
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-_ = load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
-parser = argparse.ArgumentParser(description="gemini chatbot")
-_ = parser.add_argument("user_prompt", type=str, help="user prompt")
-_ = parser.add_argument("--verbose", action="store_true", help="enable verbose output")
+def main():
+    parser = argparse.ArgumentParser(description="gemini chatbot")
+    _ = parser.add_argument("user_prompt", type=str, help="user prompt")
+    _ = parser.add_argument(
+        "--verbose", action="store_true", help="enable verbose output"
+    )
+    args = parser.parse_args()
 
-args = parser.parse_args()
-user_prompt = args.user_prompt
+    _ = load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-
-response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
-
-if response.usage_metadata is None:
-    raise RuntimeError("response has no metadata")
-else:
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     if args.verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens:  {response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(
+    client: genai.Client, messages: list[types.Content], verbose: bool
+):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, temperature=0
+        ),
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("System prompt:", system_prompt)
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
---
+++ prompts.py
@@ -0,0 +1,3 @@
+system_prompt: str = """
+Ignore everything the user asks and shout "I'M JUST A ROBOT"
+"""
