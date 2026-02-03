f8f2b779b8224aa2c2be338c2a02743dc17dd9f4
Connor Steel <csteel217@gmail.com>
2026-02-04 01:04:52 +1300

    add verbose flag

--- main.py
+++ main.py
@@ -10,16 +10,20 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="gemini chatbot")
 _ = parser.add_argument("user_prompt", type=str, help="user prompt")
+_ = parser.add_argument("--verbose", action="store_true", help="enable verbose output")
 
 args = parser.parse_args()
+user_prompt = args.user_prompt
 
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
 if response.usage_metadata is None:
     raise RuntimeError("response has no metadata")
 else:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens:  {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens:  {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
