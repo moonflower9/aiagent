163dd6ac0152b169f4562f58a3140c9157dc1eed
moon <csteel217@gmail.com>
2026-02-04 11:44:43 +1300

    create script execution method

---
+++ functions/write_file.py
@@ -0,0 +1,19 @@
+import os
+
+
+def write_file(working_directory: str, file_path: str, content: str):
+    try:
+        abs_working_dir: str = os.path.abspath(working_directory)
+        abs_file_path: str = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(abs_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        os.makedirs(abs_working_dir, exist_ok=True)
+        with open(abs_file_path, "w") as f:
+            _ = f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f"Error writing to file: {e}"
